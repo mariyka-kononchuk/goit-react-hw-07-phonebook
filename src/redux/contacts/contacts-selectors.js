66e00d71d00688838d50c841a54955270780ff5e
Maria Kononchuk <77247240+mariyka-kononchuk@users.noreply.github.com>
2021-10-14 21:18:58 +0300

    selector memoization added

--- src/redux/contacts/contacts-selectors.js
+++ src/redux/contacts/contacts-selectors.js
@@ -1,13 +1,15 @@
+import {createSelector} from '@reduxjs/toolkit'
+
 export const getContacts = state => state.contacts.items;
 
 export const getFilter = state => state.contacts.filter;
 
-export const getVisibleContacts = state => {
-    const allContacts = getContacts(state);
-    const filter = getFilter(state);
-
-    const normilizedFilter = filter.toLowerCase();
-
-    return allContacts.filter(contact =>
-      contact.name.toLowerCase().includes(normilizedFilter));
-  }
+export const getVisibleContacts = createSelector(
+    [getContacts, getFilter],
+    //тело функции будет выполнено только если изменится contacts или filter
+    (contacts, filter) => {
+        const normilizedFilter = filter.toLowerCase();
+        return contacts.filter(contact =>
+            contact.name.toLowerCase().includes(normilizedFilter));
+    }
+)   
