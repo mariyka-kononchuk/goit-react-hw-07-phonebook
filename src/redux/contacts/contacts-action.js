1ca5680283fbe769b60505bfd5029063504ce153
Maria Kononchuk <77247240+mariyka-kononchuk@users.noreply.github.com>
2021-10-14 11:39:06 +0300

    api-service added

--- src/redux/contacts/contacts-action.js
+++ src/redux/contacts/contacts-action.js
@@ -2,13 +2,17 @@
 import { createAction } from '@reduxjs/toolkit';
 import { v4 as uuidv4 } from 'uuid';
 
-export const addContact = createAction('app/addContact', ({name,number}) => ({
-    payload: {
-        id: uuidv4(),
-        name,
-        number
-    }   
-}))
+const addContact = ({ name, number }) => dispatch => {
+    
+}
+
+// export const addContact = createAction('app/addContact', ({name,number}) => ({
+//     payload: {
+//         id: uuidv4(),
+//         name,
+//         number
+//     }   
+// }))
 
 export const deleteContact = createAction('app/deleteContact');
 export const changeFilter = createAction('app/changeFilter');
---
+++ src/redux/contacts/contacts-api.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+axios.defaults.baseURL = 'http://localhost:4040';
+
+const fetchContacts = () => {
+    return axios.get('/contacts').then(response => response.data);
+};
+
+const addContact = contact => {
+    return axios.post('/contacts', contact).then(({ data }) => data);
+};
+
+const deleteContact = contactId => {
+    return axios.delete(`/contacts/${contactId}`);
+};
+
+const updateContact = (contactId, update) => {
+    return axios.patch(`/contacts/${contactId}`, update).then(({ data }) => data);
+};
+
+export { fetchContacts, addContact, deleteContact, updateContact };
